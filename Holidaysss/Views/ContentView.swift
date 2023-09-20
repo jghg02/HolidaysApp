1eaa977b74a9323eb50d5a50a6e883b1f6a30730
Josue Hernandez <jghg.02@gmail.com>
2023-09-20 12:33:25 -0300

    Add NavigationSplitView

--- Holidaysss/Views/ContentView.swift
+++ Holidaysss/Views/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @ObservedObject  private var viewModel: HolidayViewModel
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
 
+    @State private var columnVisibility =   NavigationSplitViewVisibility.doubleColumn
+
     init()  {
         self.viewModel = HolidayViewModel(getHolidaydUseCase: GetHolidaysUseCase(repository: HolidayRepositoryImp()))
     }
@@ -21,11 +23,21 @@ struct ContentView: View {
         Group {
             if horizontalSizeClass == .regular {
                 // iPad
-                CalendarView(holidays: self.viewModel.holidays)
-                    .navigationViewStyle(StackNavigationViewStyle()) // Use stack style for iPad
+                NavigationSplitView(columnVisibility: $columnVisibility) {
+                    HolidayListView(holidays: self.viewModel.holidays,
+                                    nextHoliday: self.viewModel.nextHoliday ?? Holiday.defaultValue)
+                    .navigationTitle("holidays".localized)
                     .task {
                         await self.viewModel.getAllHolidays(by: dateViewModel.currentYear)
+                        self.viewModel.getNextHoliday()
                     }
+                } detail: {
+                    Text("DETAIKL")
+                }
+                // This style reduces the size of the detail view to make
+                // room to show the leading column(s) side-by-side
+                .navigationSplitViewStyle(.balanced)
+
             } else {
                 // iPhone
                 HolidayListView(holidays: self.viewModel.holidays,
