2a661d7681179753205dc8e3d9ba35fa214dfe37
Josue Hernandez <jghg.02@gmail.com>
2023-09-27 15:59:54 -0300

    Change spalsh animation

--- Holidaysss/Views/SplashView.swift
+++ Holidaysss/Views/SplashView.swift
@@ -11,19 +11,28 @@ struct SplashView: View {
 
     @State var isActive: Bool = false
 
+    // Define your gradient
+    let gradient: LinearGradient = LinearGradient(
+        gradient: Gradient(colors: [Color.blue, Color.white, Color.red]),
+        startPoint: .top,
+        endPoint: .bottom)
+
     var body: some View {
         ZStack {
+            // Use gradient as background
+            gradient.edgesIgnoringSafeArea(.all)
             if self.isActive {
                 ContentView()
             } else {
-                LottieView(loopMode: .loop, resource: "wine")
+                LottieView(loopMode: .loop, resource: "chile")
                     .scaleEffect(0.6)
             }
         }
+
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
                 withAnimation {
-                    self.isActive = true
+                    self.isActive = false
                 }
             }
         }
